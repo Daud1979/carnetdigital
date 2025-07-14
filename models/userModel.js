59ad883df59fb05978933c172b40202a2d697a0f
Daud1979 <daudpead@hotmail.com>
2025-07-14 13:50:48 +0200

    fecha arreglado

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const getUserByCredentials = async (email, dni, birthdate) => {
     const result = await sql.query`
       select id=ma.NumIdMatricula,nombre=tg.strDescripcion 
       from Alumnos al inner join Matriculas ma on (al.numIdAlumno=ma.numIdAlumno) inner join matriculagrupos magru on (ma.numIdMatricula=magru.numIdMatricula) inner join grupos gru on (magru.numIdGrupo=gru.numIdGrupo)  inner join tiposgrupos tg on (gru.numIdTipoGrupo=tg.numIdTipoGrupo) 
-      where tg.numIdTipoGrupo <>1 and tg.numIdTipoGrupo<> 39 and al.strNif <> '' and al.strNif=ltrim(rtrim(${dni})) and convert(varchar(10),fecFechaNacimiento,103)=convert(varchar(10),convert(date,${birthdate}),103) and strEmail=ltrim(rtrim(${email}))
+      where tg.numIdTipoGrupo in (2,3,7,8,9,10,11,12,15,16,18,19,20,21,27,28,31,33,41,45) and al.strNif <> '' and al.strNif=ltrim(rtrim(${dni})) and convert(varchar(10),fecFechaNacimiento,103)=convert(varchar(10),convert(date,${birthdate}),103) and strEmail=ltrim(rtrim(${email}))
     `;
     return result.recordset;
   } catch (error) {
@@ -16,8 +16,8 @@ const getUserByCredentials = async (email, dni, birthdate) => {
 const datoscarnet = async (idMatricula) => {
   try {
     const result = await sql.query`select gru.numIdTipoGrupo,TDocumento=strNif,Nombre= strNombre,Apellidos=strApellidos,Fecha_Valido=convert(varchar(10),fecFechaMatricula,103),Expira=convert(varchar(10),dateadd(year,5,fecFechaMatricula),103),N_Registroa=ma.NumIdMatricula,Asignatura=asg.strNombreAsignatura,Titulo=tg.strDescripcion,Foto=strfoto,Tipo = CASE 
-              WHEN tg.strDescripcion LIKE '%RD%' THEN 'RD'
-              ELSE 'UNE' 
+              WHEN tg.strDescripcion LIKE '%RD%' THEN 'RD'  WHEN tg.strDescripcion LIKE '%UNE%' THEN 'UNE'
+              ELSE 'RD' 
            END
 from Alumnos al   inner join Matriculas ma on (al.numIdAlumno=ma.numIdAlumno) 
 				  inner join matriculagrupos magru on (ma.numIdMatricula=magru.numIdMatricula)
@@ -26,7 +26,7 @@ from Alumnos al   inner join Matriculas ma on (al.numIdAlumno=ma.numIdAlumno)
 				 inner join clases clas on (clas.numIdClase=clagru.numIdClase)
 				 inner join Asignaturas asg on (clas.numIdAsignatura=asg.NumIdAsignatura)
 				 inner join tiposgrupos tg on (gru.numIdTipoGrupo=tg.numIdTipoGrupo)
-where (ma.numIdMatricula=${idMatricula}) and strFoto is not null`;
+where (ma.numIdMatricula=${idMatricula}) and tg.numIdTipoGrupo in (2,3,7,8,9,10,11,12,15,16,18,19,20,21,27,28,31,33,41,45) and strFoto is not null`;
     return result.recordset;
   } catch (error) {
     console.error('Error al buscar usuario:', error);
