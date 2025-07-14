4de29d12ca9c7c72fd4c132a3fab2b2f44327b5a
Daud1979 <daudpead@hotmail.com>
2025-07-14 18:22:26 +0200

    casi carnet completo

--- public/js/slider.js
+++ public/js/slider.js
@@ -22,35 +22,71 @@ carnetFlip.addEventListener('click', () => {
       if (carnet) {
         e.preventDefault();
         const id = carnet.dataset.id;   
-        document.getElementById('titulos').textContent    = 'CARGANDO DATOS... ESPERE';               
+        document.getElementById('fondocarnets').style.backgroundImage = 'none';
+        document.getElementById('fondocarnets2').style.backgroundImage = `none`;
+        document.getElementById('titulos').textContent    = '';               
         document.getElementById('expiras').textContent    = '';
+        document.getElementById('nombre').textContent    = '';
+        document.getElementById('apellidos').textContent    = '';
+        document.getElementById('doc').textContent    = '';
         document.getElementById('validos').textContent    = '';    
         document.getElementById('nregistros').textContent = '';    
-        document.getElementById('titulo2').textContent='';
+        document.getElementById('titulo2').textContent='';   
+        document.getElementById('imgfoto').style.display = 'none';
+        document.getElementById('labeltitulo').style.display = 'none';
+        document.getElementById('labelnombre').style.display = 'none';
+        document.getElementById('labelapellidos').style.display = 'none';
+        document.getElementById('labeldni').style.display = 'none';
+        document.getElementById('valido').style.display = 'none';
+        document.getElementById('expira').style.display = 'none';
         const contenedor = document.getElementById('carnet-content2');
-        contenedor.innerHTML = '';
+        contenedor.innerHTML = '';                    ///
+        const nuevoLabel = document.createElement("label");
+        const nuevoLabel2 = document.createElement("label");
+        nuevoLabel.className = "clasefoto";
+        nuevoLabel2.className = "clasefoto";
+        document.getElementById("fondocarnets").appendChild(nuevoLabel);
+        document.getElementById("fondocarnets2").appendChild(nuevoLabel2);
+        let puntos = 0;
+        const intervalo = setInterval(() => {
+          puntos = (puntos + 1) % 4; // 0..3
+          nuevoLabel.textContent = "CARGANDO DATOS DEL ALUMNO" + ".".repeat(puntos);
+          nuevoLabel2.textContent = "CARGANDO DATOS DEL ALUMNO" + ".".repeat(puntos);
+        }, 500);
+        ///
          try {
-    const response = await fetch(`/carnet/${id}`);
-    const carnet = await response.json();
-    // Cambiar fondoc según Tipo
-    
-    const fondo = carnet[0].Tipo === 'UNE' ? 'anversoUNE.png' : 'anversoNormal.png';
-    const fondo2 = carnet[0].Tipo === 'UNE' ? 'reversoUNE.png' : 'reversoNormal.png';
-    const titulo2 = carnet[0].Tipo === 'UNE' ? 'Homologación UNE 58451 ES153333-1' : '';
-    document.getElementById('fondocarnets').style.backgroundImage = `url('/images/${fondo}')`;
-  document.getElementById('fondocarnets2').style.backgroundImage = `url('/images/${fondo2}')`;
-    // Mostrar datos
-
-    document.getElementById('titulos').textContent    = carnet[0].Titulo;
-    document.getElementById('nregistros').textContent = carnet[0].N_Registroa
-    document.getElementById('expiras').textContent    = carnet[0].Expira;
-    document.getElementById('validos').textContent    = carnet[0].Fecha_Valido;    
-    document.getElementById('titulo2').textContent    = titulo2;
-carnet.forEach(c => {
-  const label = document.createElement('label');
-  label.textContent = c.Asignatura;
-  contenedor.appendChild(label);
-});
+            const response = await fetch(`/carnet/${id}`);
+            const carnet = await response.json();
+            // Cambiar fondoc según Tipo
+            clearInterval(intervalo);
+            nuevoLabel.remove();
+            nuevoLabel2.remove();
+            const fondo = carnet[0].Tipo === 'UNE' ? 'anversoUNE.png' : 'anversoNormal.png';
+            const fondo2 = carnet[0].Tipo === 'UNE' ? 'reversoUNE.png' : 'reversoNormal.png';
+            const titulo2 = carnet[0].Tipo === 'UNE' ? 'Homologación UNE 58451 ES153333-1' : '';
+            document.getElementById('fondocarnets').style.backgroundImage = `url('/images/${fondo}')`;
+            document.getElementById('fondocarnets2').style.backgroundImage = `url('/images/${fondo2}')`;
+            // Mostrar datos
+            document.getElementById('titulos').textContent    = carnet[0].Titulo;
+            document.getElementById('nombre').textContent = carnet[0].Nombre;
+            document.getElementById('apellidos').textContent = carnet[0].Apellidos
+            document.getElementById('doc').textContent = carnet[0].TDocumento;
+            document.getElementById('nregistros').textContent = carnet[0].N_Registroa
+            document.getElementById('expiras').textContent    = carnet[0].Expira;
+            document.getElementById('validos').textContent    = carnet[0].Fecha_Valido;    
+            document.getElementById('titulo2').textContent    = titulo2;
+            document.getElementById('imgfoto').style.display = 'inline';
+            document.getElementById('labeltitulo').style.display = 'inline';
+            document.getElementById('labelnombre').style.display = 'inline';
+            document.getElementById('labelapellidos').style.display = 'inline';
+            document.getElementById('labeldni').style.display = 'inline';
+            document.getElementById('valido').style.display = 'inline';
+            document.getElementById('expira').style.display = 'inline';
+            carnet.forEach(c => {
+              const label = document.createElement('label');
+              label.textContent = c.Asignatura;
+              contenedor.appendChild(label);
+            });
   } catch (error) {
     console.error('Error al cargar carnet:', error);
   }
