f78e92ce869f7307953fd660fc3ffa46f6aeb033
Daud1979 <daudpead@gmail.com>
2025-07-18 13:17:08 +0200

    18072025 con fondos

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/authController');
 router.get('/', authController.showLogin);
 router.get('/login', authController.showLogin);
 router.post('/credenciales', authController.login);
+router.get('/credenciales', authController.login);
 router.get('/logout', authController.logout);
 router.get('/carnet/:id', authController.getCarnetPorId);
 module.exports = router;
